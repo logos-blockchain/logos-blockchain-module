a61a941bd6bc74d3ce74159a9eecbfa100691fbd
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-04-17 15:41:20 +0200

    Update flake to node version with latest FFIs.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     logos-liblogos.url = "github:logos-co/logos-liblogos";
     logos-core.url = "github:logos-co/logos-cpp-sdk";
 
-    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=feat/c-bindings/get-block-and-tx";
+    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?rev=86e3e86f78bc3a7219b1f1d169f31854dbb7344b"; # v0.2.1 + new FFIs
 
     logos-module-viewer.url = "github:logos-co/logos-module-viewer";
   };
