44c99b39624d179006ac2843ff42b078430c142d
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-04-17 10:56:02 +0200

    Rollback specific branch change.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     logos-liblogos.url = "github:logos-co/logos-liblogos";
     logos-core.url = "github:logos-co/logos-cpp-sdk";
 
-    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=feat/c-bindings/blend";
+    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=0.2.1";
 
     logos-module-viewer.url = "github:logos-co/logos-module-viewer";
   };
