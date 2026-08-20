93c5b16e43f7f3f94347fbeba46bb1dbbb8f5c56
markoburcul <marko@status.im>
2026-08-20 10:26:03 +0200

    feat(flake): add nix binary cache
    Used by CI to store build artifacts.
    
    Signed-off-by: markoburcul <marko@status.im>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,12 @@
 {
   description = "Logos Blockchain Module - Qt6 Plugin";
 
+  # Pull pre-built artifacts from the self-hosted Logos Attic cache(Nix binary cache).
+  nixConfig = {
+    extra-substituters = [ "https://cache.nix.logos.co/public" ];
+    extra-trusted-public-keys = [ "public:l4HrXgL4nw246+LBh2SOJyhz64BoGegOYLheT/iIAPU=" ];
+  };
+
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder/0.2.6";
     logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=0.2.1";
