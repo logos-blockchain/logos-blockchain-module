c18f8e7068c848d6f26cb25fbdcf1d1227299d73
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-09-25 11:32:13 +0200

    Point LB at the deployment info branch.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder/0.3.1";
-    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=master";
+    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=feat/ffi/deployment-info";
   };
 
   outputs = inputs@{ self, logos-module-builder, ... }:
